6a577262b38ea409e3a46d8b571a9804d162e53e
prolorenzo <prolorenzo.a@gmail.com>
2019-12-03 17:01:08 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,12 +25,4 @@ module "instances" {
     
     module "jenkins" {
   source                = "./modules/jenkins/"
-  key-name              = module.security.key-name
-  id-sg-bastion         = module.security.id-sg-bastion
-  id-sg-private         = module.security.id-sg-private
-  id-sg-mongodb         = module.security.id-sg-mongodb
-  subnet-pub-a-id       = module.network.subnet-pub-a-id
-  subnet-pub-b-id       = module.network.subnet-pub-b-id
-  subnet-db-a-id        = module.network.subnet-db-a-id
-  mongodb-server-domain = module.network.mongodb-server-domain
 }
