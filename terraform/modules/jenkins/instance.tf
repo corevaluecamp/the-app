3523aeb5bbfd09efdf30e97715717b79675af908
prolorenzo <prolorenzo.a@gmail.com>
2019-12-03 18:21:05 +0200

    Update instance.tf

--- terraform/modules/jenkins/instance.tf
+++ terraform/modules/jenkins/instance.tf
@@ -1,12 +1,3 @@
-	data "template_file" "install-jenkins" {
-	template = "${file("./module/jenkins/install_jenkins.sh")}"
-	vars = {
-    jenkins_user = "1"
-	jenkins_pass = "1"
-	
-  }
-}
-
 resource "aws_instance" "jenkins" {
     ami = "${var.instance-ami[2]}"
     instance_type = "t2.micro"
@@ -16,25 +7,5 @@ resource "aws_instance" "jenkins" {
     associate_public_ip_address = true
     source_dest_check = false
 
-	user_data = "${data.template_file.install-jenkins.rendered}"
 
-    tags = {
-        Name = "Jenkins"
-    }
-}
-
-resource "aws_autoscaling_group" "jenkins-asg" {
-  desired_capacity    = 1
-  max_size            = 1
-  min_size            = 1
-  vpc_zone_identifier = ["${var.subnet-pub-a-id}", "${var.subnet-pub-b-id}"]
-  launch_template {
-    id      = "${aws_instance.jenkins.id}"
-    version = "$Latest"
-  }
-}
-
-resource "aws_eip" "jenkins" {
-    instance = "${aws_instance.jenkins.id}"
-    vpc = true
 }
