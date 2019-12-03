1065038a18c7e1cdb1cbcf52a8ea5a2c2c5cb838
2g00d <>
2019-12-03 20:13:19 +0200

    add req

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,8 @@
 # SET PROVIDER - AMAZON WEB SERVICES
+terraform {
+  required_version = "> 0.12.08"
+}
+
 provider "aws" {
   region = "${var.aws-region}"
 }
@@ -22,22 +26,22 @@ module "instances" {
   subnet-db-a-id        = module.network.subnet-db-a-id
   mongodb-server-domain = module.network.mongodb-server-domain
 }
-    
+
 module "jenkins" {
-  source                = "./modules/jenkins/"
-  id-sg-bastion         = module.security.id-sg-bastion
-	key-name              = module.security.key-name
-	subnet-pub-a-id       = module.network.subnet-pub-a-id
-	jenkins_user1 = 1
+  source          = "./modules/jenkins/"
+  id-sg-bastion   = module.security.id-sg-bastion
+  key-name        = module.security.key-name
+  subnet-pub-a-id = module.network.subnet-pub-a-id
+  jenkins_user1   = 1
 }
 
 module "backend" {
-  source            = "./modules/backend"
-  key               = module.security.key-name
-  s3_bucket_name    = "${var.s3_bucket_name}"
+  source         = "./modules/backend"
+  key            = module.security.key-name
+  s3_bucket_name = "${var.s3_bucket_name}"
   #my_private_subnet = ""
   #my_public_subnet  = ""
   #my_sg             = ""
-  my_vpc            = module.network.vpc-id
-  mongo_ip          = module.instances.mongo-server-ip
+  my_vpc   = module.network.vpc-id
+  mongo_ip = module.instances.mongo-server-ip
 }
