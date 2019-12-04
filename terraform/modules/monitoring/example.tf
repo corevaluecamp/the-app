01f33c02e4aaf8b69957f872e52f3823746c018e
nordcave <nadraga.oleg@gmail.com>
2019-12-04 13:14:30 +0200

    Removed majority of testing stuff

--- terraform/modules/monitoring/example.tf
+++ terraform/modules/monitoring/example.tf
@@ -1,99 +1,19 @@
-variable "region" {} 
-variable "shared_credentials_file" {}
-variable "profile" {}
-variable "my_ami" {
-  type = "map"
-}
-variable "subnet-def-3-ID" {}
-variable "my_IP" {}
 
-
-provider "aws" {
-  region                  = "${var.region}"
-  shared_credentials_file = "${var.shared_credentials_file}"
-  profile                 = "${var.profile}"
-}
-
-data "aws_subnet" "selected" {
-  id = "${var.subnet-def-3-ID}"
-}
 
 data "template_file" "init" {
   template = "${file("${path.module}/init.tpl")}"
 }
 
-data "template_file" "init_node" {
-  template = "${file("${path.module}/init_node.tpl")}"
-}
-
-
-resource "aws_instance" "inst-BE" {
-  ami           = "${lookup(var.my_ami, var.region)}"
-  instance_type = "t2.micro"
-  subnet_id = "${var.subnet-def-3-ID}"
+resource "aws_instance" "Monitoring" {
+  ami           = var.my_ami
+  instance_type = var.instance_type
+  key_name = var.key
   associate_public_ip_address = "true"
-  key_name = "UbuntuBase"
-  vpc_security_group_ids = [
-    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}", "${aws_default_security_group.default.id}"]
-  user_data = "${data.template_file.init_node.rendered}"
-  tags = {
-	Name = "inst-BE"
-  }
-}
-
-resource "aws_instance" "inst-M" {
-  ami           = "${lookup(var.my_ami, var.region)}"
-  instance_type = "t2.micro"
-  subnet_id = "${var.subnet-def-3-ID}"
-  associate_public_ip_address = "true"
-  key_name = "UbuntuBase"
-  vpc_security_group_ids = [
-    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}", "${aws_default_security_group.default.id}"]
   user_data = "${data.template_file.init.rendered}"
+  vpc_security_group_ids = [
+    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}"]
   tags = {
 	Name = "Monitoring"
-  }
-}
-
-resource "aws_instance" "inst-J" {
-  ami           = "${lookup(var.my_ami, var.region)}"
-  instance_type = "t2.micro"
-  subnet_id = "${var.subnet-def-3-ID}"
-  associate_public_ip_address = "true"
-  key_name = "UbuntuBase"
-  vpc_security_group_ids = [
-    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}", "${aws_default_security_group.default.id}"]
-  user_data = "${data.template_file.init_node.rendered}"
-  tags = {
-	Name = "inst-J"
-  }
-}
-
-resource "aws_instance" "inst-J1" {
-  ami           = "${lookup(var.my_ami, var.region)}"
-  instance_type = "t2.micro"
-  subnet_id = "${var.subnet-def-3-ID}"
-  associate_public_ip_address = "true"
-  key_name = "UbuntuBase"
-  vpc_security_group_ids = [
-    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}", "${aws_default_security_group.default.id}"]
-  user_data = "${data.template_file.init_node.rendered}"
-  tags = {
-	Name = "inst-J1"
-  }
-}
-
-resource "aws_instance" "inst-J2" {
-  ami           = "${lookup(var.my_ami, var.region)}"
-  instance_type = "t2.micro"
-  subnet_id = "${var.subnet-def-3-ID}"
-  associate_public_ip_address = "true"
-  key_name = "UbuntuBase"
-  vpc_security_group_ids = [
-    "${aws_security_group.HTTP_SSH_Admin.id}", "${aws_security_group.Metrics_Admin.id}", "${aws_default_security_group.default.id}"]
-  user_data = "${data.template_file.init_node.rendered}"
-  tags = {
-	Name = "inst-J2"
   }
 }
 
@@ -135,7 +55,7 @@ resource "aws_security_group" "HTTP_SSH_Admin" {
 
 resource "aws_security_group" "Metrics_Admin" {
   name = "Metrics_Admin"
-  description = "Allows Prometheus and Node Exporter outher traffic"
+  description = "Allows Prometheus, Node Exporter and Grafana outher traffic"
   vpc_id = "${data.aws_subnet.selected.vpc_id}"
 
   ingress {
@@ -158,28 +78,6 @@ resource "aws_security_group" "Metrics_Admin" {
   }
 
   tags = {
-	Name = "Metrics_Admin"
-  }
-}
-
-resource "aws_default_security_group" "default" {
-  vpc_id = "${data.aws_subnet.selected.vpc_id}"
-
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-	Name = "default"
+	  Name = "Metrics_Admin"
   }
 }
