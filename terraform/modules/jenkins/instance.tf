1e6d9988c818fa53ba7899088da8d9ea66b077ed
prolorenzo <prolorenzo.a@gmail.com>
2019-12-05 13:43:17 +0200

    Update instance.tf

--- terraform/modules/jenkins/instance.tf
+++ terraform/modules/jenkins/instance.tf
@@ -1,19 +1,32 @@
-resource "aws_instance" "jenkins" {
-    ami = "${var.instance-ami[0]}"
-    instance_type = "${var.instance-type[1]}"
-    key_name = "${var.key-name}"
-    vpc_security_group_ids = ["${var.id-sg-bastion}"]
-    subnet_id = "${var.subnet-pub-a-id}"
-    associate_public_ip_address = true
-    source_dest_check = false
-
-  user_data = templatefile("${var.userdata-path}/install_jenkins.sh", {
+resource "aws_launch_template" "jenkins-launch-tmpl" {
+  name                    = "Jenkins"
+  image_id                = "${var.instance-ami[0]}"
+  instance_type           = "${var.instance-type[1]}"
+  key_name                = "${var.key-name}"
+  vpc_security_group_ids  = ["${var.id-sg-bastion}"]
+  disable_api_termination = true
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "Jenkins"
+    }
+  }
+    user_data = templatefile("${var.userdata-path}/install_jenkins.sh", {
     jenkins_user = "1"
     jenkins_pass = "1"
   })
-	
-    tags = {
-        Name = "Jenkins"
-    }
-	
+  
+  tags = {
+    Name = "jenkins-launch-tmpl"
+  }
+}
+resource "aws_autoscaling_group" "jenkins" {
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+  vpc_zone_identifier = ["${var.subnet-pub-a-id}", "${var.subnet-pub-b-id}"]
+  launch_template {
+    id      = "${aws_launch_template.jenkins-launch-tmpl.id}"
+    version = "$Latest"
+  }
 }
