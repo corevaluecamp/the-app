f32024951f44d00252d5c4fc690b5b9242a5de20
prolorenzo <prolorenzo.a@gmail.com>
2019-12-05 13:45:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,8 @@ module "jenkins" {
   source          = "./modules/jenkins/"
   id-sg-bastion   = module.security.id-sg-bastion
   key-name        = module.security.key-name
-  subnet-pub-a-id = module.network.subnet-pub-a-id
+  subnet-pub-a-id       = module.network.subnet-pub-a-id
+  subnet-pub-b-id       = module.network.subnet-pub-b-id
   jenkins_user1   = 1
 }
 
