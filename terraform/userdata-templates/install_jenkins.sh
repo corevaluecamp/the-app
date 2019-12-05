a85a7380c3fcaa4e824defadae11e3fd595f030c
prolorenzo <prolorenzo.a@gmail.com>
2019-12-05 14:15:28 +0200

    Added filebeat

--- terraform/userdata-templates/install_jenkins.sh
+++ terraform/userdata-templates/install_jenkins.sh
@@ -116,3 +116,26 @@ systemctl start node_exporter
 
 # Enabling the node exporter service to the system startup *}
 systemctl enable node_exporter
+
+#################################
+# Installing Filebeat user-data #
+#################################
+
+#!/bin/bash
+
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cat > /etc/yum.repos.d/elasticsearch.repo <<- EOF
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+yum install filebeat -y
+sed -i -e 's/localhost:9200/${elastic_ip}:9200/g' /etc/filebeat/filebeat.yml
+sed -i -e 's/enabled: false/enabled: true/g' /etc/filebeat/filebeat.yml
+systemctl enable filebeat
+systemctl start filebeat
