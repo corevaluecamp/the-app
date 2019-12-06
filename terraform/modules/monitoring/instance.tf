e0b3ee1b2801fce829dd8a83a3a62e0cf34172c7
nordcave <nadraga.oleg@gmail.com>
2019-12-06 20:37:40 +0200

    create required roles for instance discovery

--- terraform/modules/monitoring/instance.tf
+++ terraform/modules/monitoring/instance.tf
@@ -1,5 +1,3 @@
-
-
 data "template_file" "init" {
   template = "${file("${path.module}/init.tpl")}"
 }
@@ -9,11 +7,13 @@ resource "aws_instance" "Monitoring" {
   instance_type = var.instance_type
   key_name = var.key-name
   subnet_id = var.subnet_id
-  # iam_instance_profile        = "${aws_iam_instance_profile.s3_profile.name}"
   associate_public_ip_address = "false"
   user_data = "${data.template_file.init.rendered}"
   vpc_security_group_ids = [
     "${var.id-sg-metrics}", "${var.id-sg-monitoring-access}"]
+  iam_instance_profile {
+    name = "${aws_iam_instance_profile.monitoring-profile.name}"
+  }
   tags = {
 	Name = "Monitoring"
   }
