037055fb17a91cdc0309c50cc0e1c648042b380b
Roman <romanktsu@gmail.com>
2019-12-12 19:09:26 +0200

    update Jenkinsfile front-end

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,6 @@
 # SET PROVIDER - AMAZON WEB SERVICES
 provider "aws" {
-  region = "${var.aws-region}"
-  access_key = "AKIAJPN2TCXONIFAAB5A"
-  secret_key = "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"
+  region = "${var.aws-region}" 
 }
 terraform {
   required_version = "> 0.12.08"
