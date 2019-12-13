6532dd345085865d45e15102c99083cb73697378
prolorenzo <prolorenzo.a@gmail.com>
2019-12-13 15:39:44 +0200

    Update install_jenkins.sh

--- terraform/userdata-templates/install_jenkins.sh
+++ terraform/userdata-templates/install_jenkins.sh
@@ -165,8 +165,10 @@ systemctl enable node_exporter
 # Installing Filebeat user-data #
 #################################
 
+#!/bin/bash
+# Download and install filebeat
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-cat > /etc/yum.repos.d/elasticsearch.repo <<- EOF
+cat > /etc/yum.repos.d/elasticsearch.repo << EOF
 [elasticsearch-7.x]
 name=Elasticsearch repository for 7.x packages
 baseurl=https://artifacts.elastic.co/packages/7.x/yum
@@ -178,14 +180,17 @@ type=rpm-md
 EOF
 yum update -y
 yum install filebeat -y
+# Configure filebeat
 sed -i -e 's/localhost:9200/${elastic_ip}:9200/g' /etc/filebeat/filebeat.yml
 sed -i -e 's/enabled: false/enabled: true/g' /etc/filebeat/filebeat.yml
 sed -i '29c\    - /var/log/mongodb/mongodb.log' /etc/filebeat/filebeat.yml
 sed -i '30c\    - /var/log/jenkins/jenkins.log' /etc/filebeat/filebeat.yml
 sed -i '31c\    - /home/ec2-user/logs/*.log' /etc/filebeat/filebeat.yml
-cat >> /etc/filebeat/filebeat.yml <<-EOF
-setup.ilm.rollover_alias: "jenkins"
+# Create custom index name
+cat >> /etc/filebeat/filebeat.yml << EOF
+setup.ilm.rollover_alias: "customindexname"
 setup.ilm.overwrite: true
 EOF
+# Add filebeat to startup
 systemctl enable filebeat
 systemctl start filebeat
